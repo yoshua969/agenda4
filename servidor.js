46819a26607b41f996b51e1b5d64afd35885c4f1
yoshua969 <nunezfelipe110@gmail.com>
2025-12-05 16:03:03 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -17,6 +17,7 @@ const PUERTO = process.env.PORT || 3000
 
 // Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.static("public"))
 
